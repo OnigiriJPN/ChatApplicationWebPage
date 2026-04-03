34948734d629d301d80efa8454131500bbfdb06e
OnigiriJPN <yuzuhajintian@gmail.com>
2026-04-03 16:26:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Index.js 2026/04/03記入
 const IsOnline = Navigator.onLine;
 const Dld = document.getElementById("Dld-Spin");
+const downloadBtn = document.getElementById("downloadBtn");
 // もしインターネットがオフラインだった時
 if(IsOnline === false){
   Dld.style.display = "block";
-  
+  downloadBtn.disabled = true;
 }
+else{
+  Dld.style.display = "none";
+  downloadBtn.disabled = false;
+}
+
